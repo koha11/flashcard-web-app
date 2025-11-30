20e0e5fc208cbf92408ba65165365ad4fb37f7e1
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-11-30 17:01:49 +0700

    feat(user): add query users by email

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Services\UserService;
+use Illuminate\Http\Request;
+
+class UserController extends Controller {
+
+  private UserService $service;
+
+  public function __construct(UserService $service)
+  {
+    $this->service = $service;
+  }
+
+  public function getByEmail(Request $request) {
+    $email = $request->query('email');
+    $userId = $request->user()->user->id;
+    return $this->service->getByEmail($email, $userId);
+  }
+
+}
+
+
+?>
